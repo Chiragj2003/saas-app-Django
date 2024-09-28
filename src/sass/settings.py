d06790870d14dacc5698e225bce8932b5b06198f
Chirag <chiragj2019@gmail.com>
2024-09-29 04:38:18 +0530

    adding to databases

--- src/sass/settings.py
+++ src/sass/settings.py
@@ -94,6 +94,20 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+CONN_MAX_AGE = config("CONN_MAX_AGE",cast=int, default=30)
+DATABASES_URL = config("DATABASE_URL",cast=str)
+
+if DATABASES_URL is not None :
+    import dj_database_url
+    DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASES_URL,
+        conn_max_age=30,
+        conn_health_checks=True,
+        )
+    }
+
+
 
 
 # Password validation
